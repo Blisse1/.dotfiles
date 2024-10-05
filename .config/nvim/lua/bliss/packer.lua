2d19bb246376f5e13fd0dcc26a732af1fba0a355
bliss <107047190+Blisse1@users.noreply.github.com>
2024-10-05 18:12:31 -0500

    Revert "prep for lazy"
    This reverts commit 81bf73bb3fa0589b1426e095412749e918bc93fb.

---
+++ .config/nvim/lua/bliss/packer.lua
@@ -0,0 +1,83 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use('ThePrimeagen/harpoon')
+
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- Recommended, not required.
+    -- If you are using Packer
+    use "folke/tokyonight.nvim"
+
+    use({asd
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
+
+    use ('mbbill/undotree')
+
+    use ('tpope/vim-fugitive')
+
+    use({
+        "jake-stewart/multicursor.nvim",
+        branch = "1.0",
+    })
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- (Autocompletion)
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    }
+
+    use ('tpope/vim-commentary')
+
+    use ('kana/vim-textobj-user')
+
+    use ('kana/vim-textobj-entire')
+
+    use { -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help indent_blankline.txt`
+        opts = {
+            char = '┊',
+            show_trailing_blankline_indent = false,
+        }
+    }
+
+    use ("tpope/vim-obsession")
+    -- Lua
+    use("stevearc/oil.nvim")
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+end)
