b6c8fe39dd9cc5cad0c064cdaaa747032a6a02a8
bliss <107047190+Blisse1@users.noreply.github.com>
2025-01-23 12:39:24 -0500

    solved lsp configs and oil conflict with harpoon

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,29 +1,14 @@
 return {
     'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    --- it has to load before anything else, 
+    --- otherwise netrw will load first
+    lazy = false,
+    priority = 1000,
     opts = {
-        view_options = {
-            -- Show files and directories that start with "."
-            show_hidden = true,
-            -- This function defines what is considered a "hidden" file
-            is_hidden_file = function(name, bufnr)
-                return vim.startswith(name, ".")
-            end,
-            -- This function defines what will never be shown, even when `show_hidden` is set
-            is_always_hidden = function(name, bufnr)
-                return false
-            end,
-            -- Sort file names in a more intuitive order for humans. Is less performant,
-            -- so you may want to set to false if you work with large directories.
-            natural_order = true,
-            sort = {
-                -- sort order can be "asc" or "desc"
-                -- see :help oil-columns to see which columns are sortable
-                { "type", "asc" },
-                { "name", "asc" },
-            },
-        },
         keymaps = {
-            ["g?"] = { "actions.show_help", mode = "n" },
+            ["g?"] = false,
             ["<CR>"] = "actions.select",
             ["<C-s>"] = false,
             ["<C-h>"] = false,
@@ -31,16 +16,20 @@ return {
             ["<C-p>"] = false,
             ["<C-c>"] = false,
             ["<C-l>"] = false,
-            ["<leader>pv"] = { "actions.parent", mode = "n" },
+            ["-"] = false,
             ["_"] = false,
             ["`"] = false,
             ["~"] = false,
             ["gs"] = false,
-            ["gx"] = "actions.open_external",
-            ["g."] = { "actions.toggle_hidden", mode = "n" },
-            ["g\\"] = { "actions.toggle_trash", mode = "n" },
-        },
+            ["gx"] = false,
+            ["g."] = false,
+            ["g\\"] = false,
+        }
     },
+    keys = {{ "<leader>pv", "<cmd>Oil<cr>" }},
     -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    dependencies = {
+        { "echasnovski/mini.icons", opts = {} },
+    },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
